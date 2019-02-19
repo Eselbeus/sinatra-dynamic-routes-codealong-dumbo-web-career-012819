9365b9d9d35cfa7b0bf4ad3c964f63d2d525a762
Seann Branchfield <eselbeus@gmail.com>
2019-02-19 21:42:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   
   get "/multiply/:num1/:num2" do
     @product = params[:num1] * params[:num2]
-    status_code: 200
+    status_code 200
     "#{@product}"
   end
   
